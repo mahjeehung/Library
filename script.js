9f67ba076a61512a1ee87a57667d30649cfde0de
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-08-24 17:43:40 +0800

    book display fx

--- script.js
+++ script.js
@@ -14,10 +14,41 @@ function addBookToLibrary(book) {
     library.push(book)
 }
 
-  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-  
-  console.log(theHobbit.info());
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
 
-  addBookToLibrary(theHobbit);
+const PhilosopherStone = new Book("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', '223', 'read');
 
-  console.log(library);
+// document.addEventListener('DOMContentLoaded', addBookToLibrary(theHobbit));
+// document.addEventListener('DOMContentLoaded', showLibrary());
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(PhilosopherStone);
+
+const container = document.getElementById('container');
+
+library.forEach((book)=> {
+    let content = `
+      <div class="card">
+        <div class="cardContent">
+          <h4><b>'${book.title}'</b></h4>
+          <p>Author : ${book.author}</p>
+          <p>Pages : ${book.pages}</p>
+          <p>Read? : ${book.read}</p>
+        </div>
+      </div>`
+    container.innerHTML += content; })
+
+
+// function showLibrary(){
+//   library.forEach(function(book){
+//     let card = document.createElement('div');
+//     card.classList = 'card';
+
+//     let content = `
+//       <div class="container">
+//         <h4><b>'${this.title}'</b></h4>
+//       </div>`
+
+//     card.innerHTML += content;
+//   })
+// }
