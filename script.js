a051d7c23c9b26efedde557187265a5209f5fea4
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-09-18 18:49:11 +0800

    Completion
    Completed

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
-const library = [];
+let library = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
   }
+}
   
 function addBookToLibrary(book) {
-    library.push(book)
+  library.push(book)
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'no'));
+addBookToLibrary(new Book("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', '223', 'yes'));
 
-const PhilosopherStone = new Book("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', '223', 'read');
+function isBookRead(book){
+  if (book.read=='yes'){
+      return `Read`;
+  }   else    {
+      return `Not Read Yet`;
+  }
+}
 
-// document.addEventListener('DOMContentLoaded', addBookToLibrary(theHobbit));
-// document.addEventListener('DOMContentLoaded', showLibrary());
-
-addBookToLibrary(theHobbit);
-addBookToLibrary(PhilosopherStone);
-
-const container = document.getElementById('container');
-
-library.forEach((book)=> {
+function refreshLibrary(){
+  let removeDOM = document.getElementsByClassName('card');
+  for (let i = 0; 0 <removeDOM.length; i){
+    removeDOM[i].remove();
+  } 
+  library.forEach((book)=> {
     let content = `
       <div class="card">
         <div class="cardContent">
           <h4><b>'${book.title}'</b></h4>
           <p>Author : ${book.author}</p>
           <p>Pages : ${book.pages}</p>
-          <p>Read? : ${book.read}</p>
+          <button class="removeBook"  data-index-number="${library.indexOf(book)}">Remove this book</button>
+          <button class='readStatus' data-index-number='${library.indexOf(book)}'>${isBookRead(book)}</button>
         </div>
       </div>`
-    container.innerHTML += content; })
+      document.getElementById('container').innerHTML += content 
+  });
 
+let removeBookBtn = document.getElementsByClassName("removeBook");
+  for (let i = 0; i <removeBookBtn.length; i++){
+    removeBookBtn[i].addEventListener("click",removeBookFx);
+  }
+function removeBookFx(event){
+  let getATTRIBUTE = event.target.getAttribute("data-index-number");
+  library = library.slice(0, `${getATTRIBUTE}`).concat(library.slice(`${getATTRIBUTE}`+1));
+  refreshLibrary();
+}
 
-// function showLibrary(){
-//   library.forEach(function(book){
-//     let card = document.createElement('div');
-//     card.classList = 'card';
+let readStatusBtn = document.getElementsByClassName('readStatus');
+    for (let i = 0; i <readStatusBtn.length; i++){
+        readStatusBtn[i].addEventListener("click",toggleReadStatusFx);
+    }
+}
+function toggleReadStatusFx(event){
+    let getATTRIBUTE = event.target.getAttribute("data-index-number");
+    if (library[`${getATTRIBUTE}`].read=='yes'){
+      library[`${getATTRIBUTE}`].read='no';
+        event.target.textContent = 'Not Read Yet'
+    }   else    {
+      library[`${getATTRIBUTE}`].read='yes';
+        event.target.textContent = `Read`
+    }
+}
 
-//     let content = `
-//       <div class="container">
-//         <h4><b>'${this.title}'</b></h4>
-//       </div>`
+const newBookBtn = document.getElementById("newBookBtn");
+const newBookDialog = document.getElementById("newBookDialog")
+newBookBtn.addEventListener("click", () => {
+  newBookDialog.showModal();
+});
 
-//     card.innerHTML += content;
-//   })
-// }
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let newBookTitle = document.getElementById('title').value;
+  let newBookAuthor = document.getElementById('author').value;
+  let newBookPages = document.getElementById('pages').value;
+  let newBookRead = document.getElementById('read').value;
+  let newBook = new Book(newBookTitle,newBookAuthor,newBookPages,newBookRead);
+  addBookToLibrary(newBook);
+  newBookDialog.close();
+  refreshLibrary();
+});
+
+refreshLibrary();
